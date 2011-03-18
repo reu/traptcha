5aa626c4ffc14ca762eb549d83ca4f335f289059
Rodrigo Navarro <rnavarro1@gmail.com>
2011-03-18 11:22:01 -0300

    Include is executed on the on_load class scope

--- lib/traptcha/engine.rb
+++ lib/traptcha/engine.rb
@@ -4,11 +4,11 @@ module Traptcha
   class Engine < Rails::Engine
     initializer "traptcha" do |app|
       ActiveSupport.on_load(:action_controller) do
-        ActionController::Base.send :include, Traptcha::ControllerHelpers
+        include Traptcha::ControllerHelpers
       end
 
       ActiveSupport.on_load(:action_view) do
-        ActionView::Base.send :include, Traptcha::ViewHelpers
+        include Traptcha::ViewHelpers
       end
     end
   end
