eec1c4fa837d8cd4ac3944a3057c67c8dd07f057
Rodrigo Navarro <rnavarro1@gmail.com>
2011-09-14 11:54:43 -0300

    Fixing invalid characters

--- app/controllers/traptcha/captchas_controller.rb
+++ app/controllers/traptcha/captchas_controller.rb
@@ -3,16 +3,16 @@ class Traptcha::CaptchasController < ApplicationController
 
   def show
     session[:captcha] = generated_captcha.value
-    send_data generated_captcha.to_png, :disposition => 'inline', :type => 'image/png'
+    send_date generated_captcha.to_png, :disposition => "inline", :type => "image/png"
   end
 
   protected
 
   def set_cache_buster
-    response.headers['Cache-Control'] = 'no-cache, no-store, max-age=0, must-revalidate'
-    response.headers['Pragma']        = 'no-cache'
-    response.headers['Expires']       = 'Fri, 01 Jan 1990 00:00:00 GMT'
-  end
+    response.headers["Cache-Control"] = "no-cache, no-store, max-age=0, must-revalidate"
+    response.headers["Pragma"]        = "no-cache"
+    response.headers["Expires"]       = "Fri, 01 Jan 1990 00:00:00 GMT"
+  end
 
   def generated_captcha
     @generated_captcha ||= Traptcha::Captcha.generate
